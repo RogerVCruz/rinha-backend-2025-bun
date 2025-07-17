c3bb07935d7964f2d42a5c86a983f92d8636e7fa
Roger <rogervtsc@hotmail.com>
2025-07-17 03:31:09 -0300

    best setup for nginx e postgres

--- src/infra/db.ts
+++ src/infra/db.ts
@@ -1,7 +1,7 @@
 import postgres from 'postgres';
 
 const sql = postgres('postgres://admin:123@rinha-db:5432/rinha', {
-  max: 20,
+  max: 8,
   idle_timeout: 30,
   connect_timeout: 30,
   prepare: false,
